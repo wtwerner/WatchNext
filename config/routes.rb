8d4033f0ab25931f88f76df16793bacc55a260d0
wtwerner <wtwerner@gmail.com>
2020-12-03 13:01:19 -0600

    Connect frontend to backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :movies
+  get '/test', to: 'application#test'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
