ca6853d915742d1b1f2978a1d934a3d648026655
wtwerner <wtwerner@gmail.com>
2020-12-04 23:40:25 -0600

    Added better seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,37 @@
-20.times do
-    tmdb_id = Faker::Number.number(digits: 6)
-    to_watch = true
-    watched = false
-    Movie.create(
-        tmdb_id: tmdb_id,
-        to_watch: to_watch,
-        watched: watched
-    )
-end
+tmdb_id = 11
+tmdb_id2 = 181808
+tmdb_id3 = 577922
+tmdb_id4 = 120
+tmdb_id5 = 89
+to_watch = true
+watched = false
+
+Movie.create(
+    tmdb_id: tmdb_id,
+    to_watch: to_watch,
+    watched: watched
+)
+
+Movie.create(
+    tmdb_id: tmdb_id2,
+    to_watch: to_watch,
+    watched: watched
+)
+
+Movie.create(
+    tmdb_id: tmdb_id3,
+    to_watch: to_watch,
+    watched: watched
+)
+
+Movie.create(
+    tmdb_id: tmdb_id4,
+    to_watch: to_watch,
+    watched: watched
+)
+
+Movie.create(
+    tmdb_id: tmdb_id5,
+    to_watch: to_watch,
+    watched: watched
+)
