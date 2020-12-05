98f3bcf3896480d1328150deb54c9b94bb2cd4ca
wtwerner <wtwerner@gmail.com>
2020-12-05 14:32:52 -0600

    Make add to watchlist functional

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,19 @@ class MoviesController < ApplicationController
         movies = Movie.all
         render json: MovieSerializer.new(movies)
     end
+
+    def create
+        movie = Movie.create(movie_params)
+        render json: MovieSerializer.new(movie)
+    end
+
+    private
+
+    def movie_params
+        params.permit(
+            :tmdb_id,
+            :watched,
+            :to_watch
+        )
+    end
 end
