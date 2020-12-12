8b263eb3321ef6c1d49b803e57bdff1f3a78ce94
wtwerner <wtwerner@gmail.com>
2020-12-12 13:44:34 -0600

    Create seed data for genres

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,35 @@
-tmdb_id = 11
-tmdb_id2 = 181808
-tmdb_id3 = 577922
-tmdb_id4 = 120
-tmdb_id5 = 89
-to_watch = true
-watched = false
+genres = {
+    0 => {"id"=>28, "name"=>"Action"},
+    1 => {"id"=>12, "name"=>"Adventure"},
+    2 => {"id"=>16, "name"=>"Animation"},
+    3 => {"id"=>35, "name"=>"Comedy"},
+    4 => {"id"=>80, "name"=>"Crime"},
+    5 => {"id"=>99, "name"=>"Documentary"},
+    6 => {"id"=>18, "name"=>"Drama"},
+    7 => {"id"=>10751, "name"=>"Family"},
+    8 => {"id"=>14, "name"=>"Fantasy"},
+    9 => {"id"=>36, "name"=>"History"},
+    10 => {"id"=>27, "name"=>"Horror"},
+    11 => {"id"=>10402, "name"=>"Music"},
+    12 => {"id"=>9648, "name"=>"Mystery"},
+    13 => {"id"=>10749, "name"=>"Romance"},
+    14 => {"id"=>878, "name"=>"Science Fiction"},
+    15 => {"id"=>10770, "name"=>"TV Movie"},
+    16 => {"id"=>53, "name"=>"Thriller"},
+    17 => {"id"=>10752, "name"=>"War"},
+    18 => {"id"=>37, "name"=>"Western"}
+}
 
-Movie.create(
-    tmdb_id: tmdb_id,
-    to_watch: to_watch,
-    watched: watched
-)
-
-Movie.create(
-    tmdb_id: tmdb_id2,
-    to_watch: to_watch,
-    watched: watched
-)
-
-Movie.create(
-    tmdb_id: tmdb_id3,
-    to_watch: to_watch,
-    watched: watched
-)
-
-Movie.create(
-    tmdb_id: tmdb_id4,
-    to_watch: to_watch,
-    watched: watched
-)
-
-Movie.create(
-    tmdb_id: tmdb_id5,
-    to_watch: to_watch,
-    watched: watched
-)
+genres.each do |genre, attributes| 
+    attributes.each do |key, val|
+        if key == "id"
+            id = val
+        elsif key == "name"
+            name = val
+        end
+        g = Genre.new
+        g.tmdb_id = id
+        g.name = name
+        g.save
+    end
+end
