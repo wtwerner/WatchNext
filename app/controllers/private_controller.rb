6fe98a5824034c4aecc5228101440a971796f326
wtwerner <wtwerner@gmail.com>
2020-12-12 18:23:26 -0600

    Create route and controller to test authorization

---
+++ app/controllers/private_controller.rb
@@ -0,0 +1,7 @@
+class PrivateController < ApplicationController
+  def test
+    render json: { 
+      message: "This is a private message for #{current_user.email} you should only see if you've got a correct token"
+    }
+  end
+end
