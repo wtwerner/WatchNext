0a1b1c0b6f321512e668604d0146a39d0d4e9856
wtwerner <wtwerner@gmail.com>
2020-12-13 21:11:02 -0600

    Add genres to create movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,7 +6,29 @@ class MoviesController < ApplicationController
     end
 
     def create
-        movie = Movie.create(movie_params)
+        movie = Movie.new
+
+        movie_params["genres"].each do |genre|
+            new_genre = Genre.new
+            new_genre.tmdb_id = genre['id']
+            new_genre.name = genre['name']
+            new_genre.movie_id = movie.id
+            new_genre.save
+            movie.genres << new_genre
+        end
+
+        movie.title = movie_params['title']
+        movie.tmdb_id = movie_params['tmdb_id']
+        movie.overview = movie_params['overview']
+        movie.poster_path = movie_params['poster_path']
+        movie.vote_count = movie_params['vote_count']
+        movie.vote_average = movie_params['vote_average']
+        movie.release_date = movie_params['release_date']
+        movie.watched = false
+        movie.to_watch = true
+
+        movie.save
+
         render json: MovieSerializer.new(movie)
     end
 
@@ -40,7 +62,7 @@ class MoviesController < ApplicationController
             :vote_average,
             :vote_count,
             :release_date,
-            :genres
+            genres: [:id, :name]
         )
     end
 end
